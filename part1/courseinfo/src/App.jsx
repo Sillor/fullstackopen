99951d382020351880b6b3a536210c92d4ce917a
Egor Strakhov <egorstrah95@gmail.com>
2026-06-06 00:37:00 -0700

    Done 1_2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,21 @@
 const Header = (props) => {
+  return <h1>{props.courseName}</h1>;
+};
+
+const Part = (props) => {
   return (
-    <>
-      <h1>{props.courseName}</h1>
-    </>
+    <p>
+      {props.part} {props.exercises}
+    </p>
   );
 };
 
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.part[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.part[2]} {props.exercises[2]}
-      </p>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
     </>
   );
 };
